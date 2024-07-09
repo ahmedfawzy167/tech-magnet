51f57880d94d2e9ffd19aa05b8a7ed8e18d45d7f
ahmed <af383628@gmail.com>
2024-07-09 16:12:19 +0300

    update line in City Model

--- app/Models/City.php
+++ app/Models/City.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name'];
 
     public function users()
     {
         return $this->hasMany(User::class);
     }
-    use HasFactory;
 }
